a94237c209c989b96d2af0d0efe9cbd39d08c0cd
robertolagreca <robertolagreca92@gmail.com>
2022-11-10 15:32:26 +0100

    Aggiunta grafica in JS per console browser

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-let kmPercorso, etaPasseggero, prezzoBiglietto, prezzoBigliettoScontato;
+let kmPercorso, etaPasseggero, prezzoBiglietto, prezzoBigliettoScontato,prezzoKm;
 
+prezzoKm = 0.21;
 kmPercorso = prompt("Inserisci i km del percorso");
 etaPasseggero = prompt("Inserisci l'età del passeggero");
 
 if (!isNaN(kmPercorso) && !isNaN(etaPasseggero)) {
 
 
-    prezzoBiglietto = (0.21 * kmPercorso).toFixed(2);
+    prezzoBiglietto = (prezzoKm * kmPercorso).toFixed(2);
 
-    console.log("Il prezzo senza applicazione di sconto è di " + prezzoBiglietto + "€");
+    console.log(` 
+    -----------------------------------------------
+    -----------------------------------------------
+    Kilometri inseriti: ${kmPercorso}------------------------
+    Eta inserita: ${etaPasseggero}-------------------------------
+    Prezzo al km : ${prezzoKm}----------------------------
+    -----------------------------------------------
+    Tariffa standard-------------------------------
+    Il prezzo senza applicazione di sconto è di---- 
+    --------------------${prezzoBiglietto} €--------------------
+    -----------------------------------------------
+    -----------------------------------------------
+    -----------------------------------------------
+    ` );
 
     prezzoBigliettoScontato = prezzoBiglietto;
 
@@ -16,14 +30,38 @@ if (!isNaN(kmPercorso) && !isNaN(etaPasseggero)) {
 
         prezzoBigliettoScontato = (prezzoBiglietto * 0.8).toFixed(2);
 
-        console.log("Il prezzo scontato del 20% è di " + prezzoBigliettoScontato + "€");
+        console.log(` 
+    -----------------------------------------------
+    Applicazione-----------------------------------
+    Tariffa under 18-------------------------------
+    Al prezzo è stato applicato uno sconto del 20%
+    --------------------${prezzoBigliettoScontato} €--------------------
+    -----------------------------------------------
+    ` );
 
     } else if (etaPasseggero > 64) {
 
-        prezzoBigliettoScontato = (prezzoBiglietto * 0.4).toFixed(2);
-        console.log("Il prezzo scontato del 40% è di " + prezzoBigliettoScontato + "€");
+        prezzoBigliettoScontato = (prezzoBiglietto * 0.6).toFixed(2);
+
+        console.log(` 
+    -----------------------------------------------
+    Applicazione-----------------------------------
+    Tariffa under 18-------------------------------
+    Al prezzo è stato applicato uno sconto del 40%
+    --------------------${prezzoBigliettoScontato} €--------------------
+    -----------------------------------------------
+    ` );
     }
 
 } else {
-    console.log("Non hai inserito in uno o più campi dei numeri. Ricorda vengono accettati solo numeri. Ricarica pagina");
+    console.log(`
+    -----------------------------------------------
+    -----------------------------------------------
+    -----------------------------------------------
+    Non hai inserito in uno o più campi dei numeri. 
+    Ricorda vengono accettati solo numeri. Ricarica 
+    pagina.----------------------------------------
+    -----------------------------------------------
+    -----------------------------------------------
+    -----------------------------------------------` );
 }
